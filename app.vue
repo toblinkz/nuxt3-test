4a083d4aaddc316a6935a439728a752deb3a525f
toblinkz <tobi.egbayelo@termii.com>
2022-05-25 10:28:51 +0100

    add

---
+++ app.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>Tobi id hgjguuuuj kolp</p>
+  </div>
+  <div>
+    <p>Tobi id hgjguuuuj kolp</p>
+  </div>
+  <div>
+    <p>Tobi id hgjguuuuj kolp</p>
+  </div>
+  <div>
+    <Test/>
+    <NuxtWelcome />
+  </div>
+  <div>
+    <p>Tobi id hgjguuuuj kolp</p>
+  </div>
+  <div>
+    <p>Tobi id hgjguuuuj kolp</p>
+  </div>
+  <div>
+    <p>Tobi id hgjguuuuj kolp</p>
+  </div>
+</template>
+<script>
+
+export default {
+
+}
+</script>
---
+++ components/Test.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>Grace is liddddfe</p>
+</template>
+
+<script>
+export default {
+  name: "Test"
+}
+</script>
+
+<style scoped>
+
+</style>
